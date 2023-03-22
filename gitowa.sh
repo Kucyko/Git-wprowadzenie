dcbbcfbe71c61aac25741ce052fdd42ef970c9dd
Kucyko <118746411+Kucyko@users.noreply.github.com>
2023-03-22 16:55:52 +0100

    Add files via upload

---
+++ gitowa.sh
@@ -0,0 +1,17 @@
+git commit
+git commit
+git branch bugFix
+git checkout bugFix
+git branch bugFix
+git checkout bugFix
+git commit
+git checkout main
+git commit
+git merge bugFix
+git brach bigFix
+git checkout bugFix
+git commit
+git checout main 
+git commit
+git checkout bugFix
+git rebase main
